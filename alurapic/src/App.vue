6d5a9327824bdf3036d2dd2a6288934bab017fd3
WallFerreira <mr.wallaceferreira@gmail.com>
2022-08-10 16:03:10 -0300

    conectando com a API

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -14,19 +14,16 @@ export default {
   data() {
     return {
       titulo: "Alurapic",
-      fotos: [
-        {
-          url: "https://img.freepik.com/fotos-gratis/lindo-retrato-de-cachorro_23-2149218451.jpg?w=1380&t=st=1660149698~exp=1660150298~hmac=b04684fef3e956df64c429d26cbb1704efc66f4344353797bd9800332ebf1057",
-          titulo: "cachorro",
-        },
-        {
-          url: "https://img.freepik.com/fotos-gratis/lindo-retrato-de-cachorro_23-2149218451.jpg?w=1380&t=st=1660149698~exp=1660150298~hmac=b04684fef3e956df64c429d26cbb1704efc66f4344353797bd9800332ebf1057",
-          titulo: "cachorro2",
-        },
-      ],
+      fotos: [],
     };
   },
-};
+
+  created (){
+    this.$http.get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+    }
+}
 </script>
 
 <style>
