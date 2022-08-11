23ded81c8f2a62c2902ca9e1b5e5e8bc33602ac7
WallFerreira <mr.wallaceferreira@gmail.com>
2022-08-11 13:40:12 -0300

    criando componente Painel e adicionando comportamento ao componente

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,30 +1,82 @@
+
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos" :key="foto">
-        <img :src="foto.url" :alt="foto.titulo" />
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="filtre por parte do título"
+    />
+    {{ filtro }}
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" :key="foto">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
 export default {
   data() {
     return {
       titulo: "Alurapic",
       fotos: [],
+      filtro: "",
     };
   },
 
-  created (){
-    this.$http.get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
-    }
-}
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then((res) => res.json())
+      .then(
+        (fotos) => (this.fotos = fotos),
+        (err) => console.log(err)
+      );
+  },
+
+  components: {
+    "meu-painel": Painel,
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i"); // 'i' case insensitive, trim() não considera espaço na string
+        return this.fotos.filter((foto) => exp.test(foto.titulo)); //para cada iteração vai ser tertada a expressão acima na lista de fotos
+      } else {
+        return this.fotos;
+      }
+    },
+  },
+};
 </script>
 
 <style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+.centralizado {
+  text-align: center;
+}
+.lista-fotos {
+  list-style: none;
+}
+.lista-fotos-item {
+  display: inline-block;
+}
+.imagem-responsiva {
+  width: 100%;
+}
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
